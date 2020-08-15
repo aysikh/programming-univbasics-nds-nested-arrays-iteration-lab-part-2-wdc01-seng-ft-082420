45629ca26f41f4ba13f0febc4aa5a6539ac93885
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-15 23:47:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,11 +3,11 @@ def find_min_in_nested_arrays(src)
   min_temperatures = []
   while row_index < src.count do
     element_index = 0
-    shortest_string_element = -1
+    shortest_string_element = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index] < shortest_string_element
+      if src[row_index][element_index] > shortest_string_element
         shortest_string_element = src[row_index][element_index]
-        shortest_string_element += 1 
+        shortest_string_element += 1
       end
       element_index += 1 
     end
